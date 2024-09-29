d4e003faea9baa969e02bd05550a077757756ea8
muammarjohnson <muammarjohnson@adsgency.ai>
2024-09-28 21:08:44 -0400

    layouts complete, svg for icons at the bottom

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,12 +3,38 @@ import React, { useEffect } from 'react'
 import { SplashScreen, Tabs } from 'expo-router'
 import { useFonts } from 'expo-font';
 import { fonts } from '@/constants/fonts';
+import { colors } from '@/constants/colors';
+import { HeartIcon, HingeIcon, MessageIcon, PersonIcon, StarIcon } from '@/constants/icons';
 
 
 
 const AppLayout = () => {
   return (
-    <Tabs>
+    <Tabs screenOptions={
+      ({route})=>({
+        tabBarStyle: { backgroundColor: colors.darkGrey1}
+      , tabBarActiveTintColor: colors.white
+      , tabBarInactiveBackgroundColor: colors.darkGrey2
+      , tabBarShowLabel: false
+      , tabBarIcon: ({color})=>{
+        switch(route.name){
+          case 'standouts': 
+            return <StarIcon fill={color}/>;
+          case 'likes': 
+            return <HeartIcon fill={color}/>;
+          case 'matches': 
+            return <MessageIcon fill={color}/>;
+          case 'account': 
+            return <PersonIcon fill={color}/>;
+          case 'index': 
+            return <HingeIcon fill={color}/>;
+          default: 
+            return null;
+        }
+      }
+      })
+
+    }>
         <Tabs.Screen name="index"/>
         <Tabs.Screen name="standouts"/>
         <Tabs.Screen name="likes"/>
---
+++ readme.txt
@@ -0,0 +1,4 @@
+1. to run:
+go to main parent folder
+type in npx run
+follow instructions
